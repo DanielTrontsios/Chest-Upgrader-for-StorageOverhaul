31adef988c456f7f364b4e9618dbddc6d5486a24
JokTronki <tronki98@yahoo.gr>
2022-04-29 11:36:44 +0300

    Update Item group to use new icon

--- src/main/java/com/tronki/upgradechestoverhaul/item/ModItemGroup.java
+++ src/main/java/com/tronki/upgradechestoverhaul/item/ModItemGroup.java
@@ -7,7 +7,7 @@ public class ModItemGroup {
   public static final ItemGroup TAB_STORAGE_UPGRADES = new ItemGroup("upgradechestoverhaulModTab") {
     @Override
     public ItemStack makeIcon() {
-      return new ItemStack(ModItems.UPGRADER.get());
+      return new ItemStack(ModItems.UPGRADER_TIER_2.get());
     }
   };
 }
